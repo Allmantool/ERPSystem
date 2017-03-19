9b4394e5fc13b45acd89a0c29ff6778dbbdd38b0
Allmantool <Allmantool@gmail.com>
2017-03-20 00:34:34 +0300

    20.03.2017 H
    Tree struct

--- ERP/Domain/Entities/KindEq.cs
+++ ERP/Domain/Entities/KindEq.cs
@@ -4,13 +4,13 @@ namespace ERP.Domain.Entities {
     /// <summary>
     /// Вид оборудования
     /// </summary>
-    public class KindEq {
+    public sealed class KindEq {
         public KindEq() {
             this.EqGroups = new HashSet<EqGroup>();
         }
         public int Id { get; set; }
         public string Description { get; set; }
 
-        public virtual ICollection<EqGroup> EqGroups { get; set; }
+        public ICollection<EqGroup> EqGroups { get; set; }
     }
 }
---
+++ ERP/Domain/Entities/StructNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERP.Domain.Entities {
+    public class StructNode {
+        public StructNode() {
+            SubModules = new List<StructNode>();
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public virtual int ParentModuleId { get; set; }
+
+        // /1/2 (path by node id)
+        public string Path { get; set; }
+        public int Depth { get; set; }
+
+        [ForeignKey("ParentModuleId")]
+        public virtual ICollection<StructNode> SubModules { get; set; }
+    }
+}
